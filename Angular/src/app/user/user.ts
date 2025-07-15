083f6ee4811c97ff9a32805648dc912b8265a36e
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2025-07-16 00:46:59 +0530

    stand alone components

---
+++ Angular/src/app/user/user.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './user.html',
+  styleUrl: './user.scss'
+})
+export class User {
+
+}
