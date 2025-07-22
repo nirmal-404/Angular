b87a8103d67b84eaaf362da11126cefc2ba859d8
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2025-07-22 14:16:47 +0530

    Set child input in ngAfterViewInit using setTimeout to fix ExpressionChangedAfterItHasBeenCheckedError

--- Angular/src/app/parent/parent.ts
+++ Angular/src/app/parent/parent.ts
@@ -1,8 +1,8 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { Child } from '../child/child';
-import { Sibling1 } from '../sibling1/sibling1';
-import { Sibling2 } from '../sibling2/sibling2';
-import { Reusable } from '../reusable/reusable';
+import { AfterViewInit, Component, ViewChild } from '@angular/core'
+import { Child } from '../child/child'
+import { Sibling1 } from '../sibling1/sibling1'
+import { Sibling2 } from '../sibling2/sibling2'
+import { Reusable } from '../reusable/reusable'
 
 @Component({
   selector: 'app-parent',
@@ -10,32 +10,32 @@ import { Reusable } from '../reusable/reusable';
   templateUrl: './parent.html',
   styleUrl: './parent.scss'
 })
-export class Parent implements AfterViewInit{
-  parentProperty : string = "Hello child - from parent";
+export class Parent implements AfterViewInit {
+  parentProperty: string = 'Hello child - from parent'
 
-  receiveGreetMsg : string = "";
+  receiveGreetMsg: string = ''
 
-  sbl1Data : any; // received data from sibling 1
-  sbl2Data : any; // received data from sibling 2
+  sbl1Data: any // received data from sibling 1
+  sbl2Data: any // received data from sibling 2
 
-  @ViewChild('reusable') reusableComponent! : Reusable;
+  @ViewChild('reusable') reusableComponent!: Reusable
 
-
-  ngAfterViewInit(): void {
-      this.reusableComponent.childProperty = "Greeting from parent component"
+  ngAfterViewInit (): void {
+    setTimeout(() => {
+      this.reusableComponent.childProperty = 'Greeting from parent component'
+    })
   }
 
-  receiveGreet(data : any) {
-    this.receiveGreetMsg = data;
+  receiveGreet (data: any) {
+    this.receiveGreetMsg = data
   }
 
-  receiveFromSBL1(data : any){
+  receiveFromSBL1 (data: any) {
     this.sbl1Data = data
     console.log(this.sbl1Data)
   }
-  receiveFromSBL2(data : any){
+  receiveFromSBL2 (data: any) {
     this.sbl2Data = data
     console.log(this.sbl2Data)
-
   }
 }
